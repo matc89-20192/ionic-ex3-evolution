2a9ecbbdb2578993c647f24d1997f259dcf86d99
RAFAELDEOLIVEIRABAHIA <rafael.bahia@email.br>
2019-11-15 22:43:58 -0300

    Finalização do Exercício 3

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { Tarefa } from '../../providers/tarefa/tarefa';
 
 @IonicPage()
 @Component({
@@ -8,8 +9,87 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class Page1 {
 
-  constructor(public navCtrl: NavController) {
+  descricao: string = "";
+  prioridade: number;
 
+  items: Array<Tarefa>;
+
+  constructor(public toastController: ToastController, public navCtrl: NavController) {
+    this.items = [];
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  validationDescricao(): boolean {
+
+    var retorno = true;
+
+    for (var i = 0; i < this.items.length; i++) {
+      if (this.items[i]["descricao"] == this.descricao) {
+        retorno = false;
+        this.presentToast('Tarefa já cadastrada.');
+        break;
+      }
+    }
+    return retorno;
+  }
+
+  validationPrioridade(): boolean {
+    if (this.prioridade >= 0 && this.prioridade <= 10) {
+      return true;
+    } else {
+      this.presentToast('A prioridade deve estar entre 1 e 10.');
+      return false;
+    }
+  }
+
+  addItem() {
+
+    if (this.validationDescricao() && this.validationPrioridade()) {
+
+      this.items.push({
+        descricao: this.descricao,
+        prioridade: this.prioridade
+      });
+
+      this.items.sort(function (obj1, obj2) {
+        // Ascending: first age less than the previous
+        return obj1.prioridade - obj2.prioridade;
+      });
+
+      this.clearInput();
+
+      this.releaseButton();
+    }
+  }
+
+  clearInput() {
+    this.prioridade = null;
+    this.descricao = "";
+  }
+
+  removerFirst() {
+    this.items.shift();
+
+    this.releaseButton();
+  }
+
+  itemTapped(event) {
+    this.items.splice(event, 1);
+    this.releaseButton();
+  }
+
+  releaseButton() {
+    if (this.items.length > 0) {
+      (<HTMLButtonElement>document.getElementById('buttonRemover')).disabled = false;
+    } else {
+      (<HTMLButtonElement>document.getElementById('buttonRemover')).disabled = true;
+    }
+  }
 }
